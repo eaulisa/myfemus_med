65f99d95ded9e884d4aaa245c7293d008734b118
Eugenio Aulisa <eaulisa@localhost.localdomain>
2023-11-21 09:35:23 +0100

    finished first coupling between Femus and medcoupling

---
+++ SRC/FemusMedCoupling.hpp
@@ -0,0 +1,101 @@
+ #pragma once
+
+// #include <FemusInit.hpp>
+// #include <MultiLevelProblem.hpp>
+// #include <MultiLevelSolution.hpp>
+// #include <VTKWriter.hpp>
+//
+
+
+using std::vector;
+
+#include <MEDCouplingFieldDouble.hxx>
+#include <MEDCouplingUMesh.hxx>
+#include <MEDLoader.hxx>
+
+
+#include <MEDCouplingNormalizedUnstructuredMesh.txx>
+//#include <MEDCouplingPointSet.hxx>
+#include <Interpolation2D.txx>
+#include <InterpKernelMatrix.hxx>
+#include <MEDCouplingRemapper.hxx>
+//#include <InterpolationPlanar.hxx>
+
+
+using namespace MEDCoupling;
+
+namespace femus {
+
+
+  class FemusMedCoupling {
+    public:
+      // Constructor
+      FemusMedCoupling(Solution *sol) {
+        _sol = sol;
+        _msh = _sol->GetMesh();
+      };
+
+      ~FemusMedCoupling() {
+        for (unsigned k = 0; k < 3; k++) {
+          if (_medMesh[k] != NULL) {
+            _medMesh[k]->decrRef();
+            _medMesh[k] = NULL;
+          }
+        }
+        for (unsigned k = 0; k < _medField.size(); k++) {
+          _medField[k]->decrRef();
+        }
+      }
+
+      void Femus2MedMesh(const unsigned &meshType = 2);
+      void Femus2MedNodeField(const std::vector<std::string> &fieldName, const unsigned &mshType = 2);
+
+      void Femus2MedMesh(const std::string &meshType = "biquadratic") {
+        if (meshType == "linear") {
+          Femus2MedMesh(0);
+        }
+        else if (meshType == "quadratic") {
+          Femus2MedMesh(1);
+        }
+        else if (meshType == "biquadratic") {
+          Femus2MedMesh(2);
+        }
+        else {
+          std::cout << "Error in void MyFEMuSMED::femus_to_med_mesh (const std::string &meshType)!!!\n";
+          std::cout << "Only linear \"0\", quadratic \"1\", or biquadratic \"2\" meshTypes are available\n";
+          abort();
+        }
+      }
+
+      void Femus2MedNodeField(const std::vector<std::string> &fieldName, const std::string &meshType = "biquadratic") {
+        if (meshType == "linear") {
+          Femus2MedNodeField(fieldName, 0);
+        }
+        else if (meshType == "quadratic") {
+          Femus2MedNodeField(fieldName, 1);
+        }
+        else if (meshType == "biquadratic") {
+          Femus2MedNodeField(fieldName, 2);
+        }
+        else {
+          std::cout << "Error in void MyFEMuSMED::femus_to_med_field (const std::vector<std::string> &fieldName, const std::string &meshType)!!!\n";
+          std::cout << "Only linear \"0\", quadratic \"1\", or biquadratic \"2\" meshTypes are available\n";
+          abort();
+        }
+      }
+
+      void BuildProjectionMatrixBetweenMeshes(const MEDCouplingUMesh *med_source_mesh, MEDCouplingUMesh *med_target_mesh);
+      void BuildProjectionMatrixBetweenFields(MEDCouplingFieldDouble *sourceField, MEDCouplingUMesh *med_target_mesh);
+
+      Solution *_sol;
+      Mesh *_msh;
+
+      MEDCoupling::MEDCouplingUMesh* _medMesh[3] = {NULL, NULL, NULL};
+      std::vector <MEDCoupling::MEDCouplingFieldDouble *> _medField;
+      std::vector <std::vector<std::string>> _medFieldNames;
+  };
+
+
+
+} // namespace femus
+
--- SRC/main.cpp
+++ SRC/main.cpp
@@ -1,23 +1,24 @@
 #include "fem_ttu.hpp"
+#include "FemusMedCoupling.hpp"
 
 using namespace femus;
 
-int main (int argc, char **args) {
+int main(int argc, char **args) {
 
   unsigned numberOfUniformRefinements = 1;
-  MyFEMuSMED myfemus (argc, args, "../MESH/square_mixed.neu", numberOfUniformRefinements);
+  MyFEMuSMED myfemus(argc, args, "../MESH/square_tri.neu", numberOfUniformRefinements);
 
-  FemusMedCoupling PM(myfemus._mlSol.GetSolutionLevel(numberOfUniformRefinements-1));
+  FemusMedCoupling PM(myfemus._mlSol.GetSolutionLevel(numberOfUniformRefinements - 1));
 
 
   unsigned mshType = 2;
-  PM.Femus2MedMesh ("linear");
-  PM.Femus2MedNodeField ({"P","V", "U"}, "linear");
-  PM.Femus2MedNodeField ({"U","P", "V"}, "biquadratic");
-  PM.Femus2MedNodeField ({"P","P", "P"}, "biquadratic");
+  PM.Femus2MedMesh("linear");
+  PM.Femus2MedNodeField({"P", "U", "V"}, "quadratic");
+  //PM.Femus2MedNodeField({"U", "P", "V"}, "biquadratic");
+  //PM.Femus2MedNodeField({"P", "P", "P"}, "biquadratic");
 
-  PM.BuildProjectionMatrixBetweenFields(PM._medField[0],PM._medMesh[0]);
-  PM.BuildProjectionMatrixBetweenMeshes(PM._medMesh[0],PM._medMesh[0]);
+  PM.BuildProjectionMatrixBetweenFields(PM._medField[0], static_cast <MEDCouplingUMesh*>(PM._medField[0]->getMesh()));
+  //PM.BuildProjectionMatrixBetweenMeshes(PM._medMesh[0],PM._medMesh[0]);
 
 
   //unsigned nIterations = 2;
